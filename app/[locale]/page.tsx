5464a376cf1fd08676606398987b15af5a12475b
cruel32 <cruel32@nate.com>
2026-02-01 01:41:47 +0900

    Update callback URLs and enhance in-app browser presentation in Home component
    - Updated the callback URL in the Home component to ensure proper navigation after social sign-in.
    - Adjusted the in-app browser presentation style to "fullscreen" for a better user experience during authentication.
    
    These changes refine the login flow and improve the overall user interface during the sign-in process.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -145,9 +145,9 @@ export default function Home() {
 
         const response = await authClient.signIn.social({
           provider: provider,
-          // callbackURL: `/${locale}/auth/login-complete`,
+          callbackURL: `/${locale}/auth/login-complete`,
           // callbackURL: `dabada://${locale}/auth/login-complete`,
-          callbackURL: `dabada://${locale}`,
+          // callbackURL: `dabada://${locale}`,
           disableRedirect: true,
         });
 
@@ -157,8 +157,9 @@ export default function Home() {
         // 인앱 브라우저 열기
         await Browser.open({
           url: response.data.url,
+          presentationStyle: "fullscreen",
         });
-
+        
         return;
       }
 
